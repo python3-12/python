eed982027dc3fff725b04d5564e437c4cab911e4
Yosef <yosefrosek5@gmail.com>
2025-03-05 16:39:58 -0500

    Create cool square.py

---
+++ images/cool square.py
@@ -0,0 +1,26 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("black")
+t.pencolor("white")
+
+a = 0  # Starting size of the side of the square
+b = 90  # Always turn 90 degrees to form a square
+t.speed(0)
+t.penup()
+t.goto(-970, 530)  # Start from top-left corner to form the square
+t.pendown()
+t.fillcolor("aqua")
+t.shape("turtle")
+while True:
+    for _ in range(4):  # Draw four sides of a square
+        t.forward(a)  # Move forward by the length of a (side of the square)
+        t.right(b)  # Turn 90 degrees after each side
+    a += 10  # Increase the side length to make the square grow large
+
+    if a > 1000:  # Stop once the square gets big enough
+        break
+
+t.hideturtle()
+turtle.done()

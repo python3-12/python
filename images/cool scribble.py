9ddff3d2cd992580d1ba9ac653ced7e507262607
Yosef <yosefrosek5@gmail.com>
2025-03-07 17:11:16 -0500

    Create cool scribble.py

---
+++ images/cool scribble.py
@@ -0,0 +1,31 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("pink")
+t.pencolor("aqua")
+
+width = 0  # Starting width of the rectangle
+height = 0  # Starting height of the rectangle
+t.speed(0)
+t.penup()
+t.goto(-970, 530)  # Start from the top-left corner
+t.pendown()
+t.fillcolor("aqua")
+t.shape("turtle")
+t.pensize(5)
+
+while True:
+    for _ in range(2):  # Draw two pairs of sides for the rectangle
+        t.forward(width)  # Move forward by the width
+        t.right(10)  # Turn 90 degrees
+        t.forward(height)  # Move forward by the height
+        t.right(90)  # Turn 90 degrees again
+    width += 14  # Increase the width of the rectangle
+    height += 9  # Increase the height of the rectangle (slower growth)
+    
+    if width > 2000:  # Stop once the width reaches a certain size
+        break
+
+t.hideturtle()
+turtle.done()

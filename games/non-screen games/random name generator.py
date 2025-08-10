d1ed81bbd7d3db3af8d1225a3993047835a1d32a
Yosef <yosefrosek5@gmail.com>
2025-08-10 16:16:44 -0400

    Delete games/non-screen games/random name generator.py

--- games/non-screen games/random name generator.py
+++
@@ -1,6 +0,0 @@
-from random import choice
-
-names = ["john","tim","tom","bob","steve","alex","lemon","daisy","logan","ash","emma","emily","mia","olivia","isabella","noah","kevin","james","ethan","william","andrew","liam","daniel","anthony","david","jacob","louis","charlote","julia","mary","amy","michelle"]
-
-random_name = choice(names)
-print(random_name)
